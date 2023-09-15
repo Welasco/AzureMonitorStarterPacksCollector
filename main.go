85df7344d6b59bd54260f85f5368ee1ded7a34c5
welasco <26201510+Welasco@users.noreply.github.com>
2023-09-15 06:29:27 +0000

    add comments

--- main.go
+++ main.go
@@ -42,19 +42,23 @@ func main() {
 	//logger.Info("Starting collectors...")
 	logger.Info("Starting collectors...")
 
+	// Create a slice of LogCollector interface
 	var logcollector []collectors.LogCollector
 	logcollector = append(logcollector, collectors.Newnginx_log(cfg))
 
-	// Graceful shutdown
+	// Graceful shutdown signals
 	ctx, stop := signal.NotifyContext(context.Background(),
 		os.Interrupt,
 		syscall.SIGTERM,
 		syscall.SIGQUIT)
 
+	// WaitGroup to wait for all goroutines to finish
 	var wg sync.WaitGroup
 	wg.Add(1)
+	// Call gracefullShutdown which gets executed when the signals SIGTERM OR SIGQUIT are received. I happens at this line under the function gracefulShutdown <-ctx.Done()
 	go gracefulShutdown(ctx, stop, &logcollector, &wg)
 
+	// Start all collectors and add them to the WaitGroup
 	for _, collector := range logcollector {
 		wg.Add(1)
 		go collector.Start(&wg)
@@ -83,18 +87,26 @@ func main() {
 	logger.Info("Reach Wait() Exiting...")
 	wg.Wait()
 	fmt.Println("Main done. Exiting...")
+
 	// Add config file for all collectors - Done
 	// Config file must support the specifics of each collector, URL, File, etc - Done
 	// Error handling everywhere - Almost Done still need to test at the go routine level to catch the error during the start of the collector
 	// add comments - Done
-	// Add a test module
 	// Add a log module - Done
-	// graceful shutdown
+	// graceful shutdown - Done
 	// add multi site nginx support
+	// add log level
+	// Add a test module
+	// Build
+	// deployment script/deb package
 
 }
 
+// Graceful shutdown function
+// It gets executed when the signals SIGTERM OR SIGQUIT are received
+// It stops all collectors and then stops the main loop
 func gracefulShutdown(ctx context.Context, stop context.CancelFunc, logcollector *[]collectors.LogCollector, wg *sync.WaitGroup) {
+	// <-ctx.Done() waits for the signals (SIGTERM OR SIGQUIT) to be received and then executes the code below
 	<-ctx.Done()
 	logger.Info("Received shutdown signal...")
 	for _, collector := range *logcollector {
